566c63a4c0079511b0afcdf92483382c5ea4147f
Tevis Gehr <tevis.gehr@gmail.com>
2017-10-10 15:19:14 -0600

    Add files via upload

---
+++ learncpp_helloworld/cmake-build-debug/CMakeFiles/learncpp_helloworld.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learncpp_helloworld.dir/main.cpp.o"
+  "learncpp_helloworld.pdb"
+  "learncpp_helloworld"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learncpp_helloworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ learncpp_helloworld/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * Multi
+ * line
+ * comment
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl; //comment
+    int x; //declaration statement
+    x = 5; //assignment statement
+    std::cout << x << std::endl;
+    return 0;
+}
