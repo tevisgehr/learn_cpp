5fe9fd77207ed6ad3e1e20d7490fa835e898a4bf
Tevis Gehr <tevis.gehr@gmail.com>
2017-10-10 15:20:49 -0600

    Add files via upload

---
+++ learncpp_cin/cmake-build-debug/CMakeFiles/learncpp_cin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learncpp_cin.dir/main.cpp.o"
+  "learncpp_cin.pdb"
+  "learncpp_cin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learncpp_cin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
