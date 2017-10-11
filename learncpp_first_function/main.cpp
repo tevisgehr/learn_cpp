80c6000b48571e17ef667752c06df30b83867334
Tevis Gehr <tevis.gehr@gmail.com>
2017-10-11 15:54:56 -0600

    function updates

--- learncpp_first_function/main.cpp
+++ learncpp_first_function/main.cpp
@@ -4,9 +4,26 @@ void doPrint() {
     std::cout << "In doPrint()" << std::endl;
 }
 
+int return5() {
+    std::cout << "return5 function" << std::endl;
+    return 5;
+}
+
+int get_y() {
+    int x;
+    std::cout << "getx function. Enter number:" << std::endl;
+    std::cin >> x;
+    return x;
+}
+
 int main() {
     std::cout << "Starting main()" << std::endl;
     doPrint();
+    int x = return5();
+    std::cout << x << std::endl;
+
+    int y = get_y();
+    std::cout << y << std::endl;
     std::cout << "Ending main()" << std::endl;
 
     return 0;
