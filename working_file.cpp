6d2b98b25c18da776541e199244224c63a78eea2
Tevis Gehr <tevis.gehr@gmail.com>
2017-10-22 09:09:58 -0600

    Creating new function for each learncpp section.

--- working_file.cpp
+++ working_file.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 
 int add(int x, int y) {
-    // Adds integers x and y together.
-    return x + y;
+  // Adds integers x and y together.
+  return x + y;
 }
 
 int multiply(int x,int y) {
-    return x * y;
+  return x * y;
 }
 
+void learn_cpp_3_4()
+{
+  int x = 2;
+  int y = 5;
+  //std::cout << multiply(add(2,1),add(x,y));
+  std::cout << sizeof(x) << std::endl;
+  int z = (1,2); //interesting result due to comma operator
+  //gives a warning when compiled with g++
+  std::cout << z << std::endl;
+}
 int main() {
-    int x = 2;
-    int y = 5;
-    //std::cout << multiply(add(2,1),add(x,y));
-    std::cout << sizeof(x) << std::endl;
-    int z = (1,2); //interesting result due to comma operator
-    //gives a warning when compiled with g++
-    std::cout << z << std::endl;
-    return 0;
+  learn_cpp_3_4();
+  return 0;
 }
